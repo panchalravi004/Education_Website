d266fa95e5c007fd4e8857f22b23af20b0db3f8e
Ravi Panchal <80586465+panchalravi004@users.noreply.github.com>
2022-06-29 15:38:19 +0530

    register and login authenticate user

--- lms/urls.py
+++ lms/urls.py
@@ -13,7 +13,6 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from unicodedata import name
 from django.contrib import admin
 from django.urls import path,include
 from lms import views, user_login
@@ -26,5 +25,6 @@ urlpatterns = [
     path('contact/', views.CONTACT,name='contact_us'),
     path('about/', views.ABOUT,name='about_us'),
     path('accounts/',include('django.contrib.auth.urls')),
-    path('accounts/register',user_login.REGISTER,name='register')
+    path('accounts/register/',user_login.REGISTER,name='register'),
+    path('dologin/',user_login.LOGIN,name='dologin')
 ]
--- lms/user_login.py
+++ lms/user_login.py
@@ -1,4 +1,47 @@
 from django.shortcuts import redirect,render
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.auth import authenticate,login,logout
+from app.EmailBackEnd import EmailBackEnd
 
 def REGISTER(request):
+
+    if request.method == "POST":
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+
+        #check the data
+        if User.objects.filter(email=email).exists():
+            messages.warning(request,"Email are Already Exists !")
+            return redirect('register')
+
+        if User.objects.filter(username=username).exists():
+            messages.warning(request,"Username are Already Exists !")
+            return redirect('register')
+
+        #register
+        user = User(
+            email=email,
+            username=username,
+            )
+        user.set_password(password)
+        user.save()
+        return redirect('login')
+
     return render(request,'registration/register.html')
+
+def LOGIN(request):
+    if request.method == "POST":
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+
+        user = EmailBackEnd.authenticate(request,username=email,password=password)
+
+        if user != None:
+            login(request,user)
+            return redirect('home')
+        else:
+            messages.error(request,'Email and Password Are Invalid !')
+            return redirect('login')
+    return None
