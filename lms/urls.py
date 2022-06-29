f7a463eadfd72e5d45f14c49f3a730e5f1381be8
Ravi Panchal <80586465+panchalravi004@users.noreply.github.com>
2022-06-29 13:36:36 +0530

    adding a header and footer

--- lms/urls.py
+++ lms/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from lms import views
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('base/', views.BASE,name='base'),
 ]
--- lms/views.py
+++ lms/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import redirect, render
 from django.http import HttpResponse
 
+def BASE(request):
+    return render(request,'base.html')
