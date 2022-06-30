bbf5747fac135a3e00538cd1f6da6d8639452b2e
Ravi Panchal <80586465+panchalravi004@users.noreply.github.com>
2022-06-30 09:09:18 +0530

    adding category and course author model created

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from app.models import Categories
 
 # Register your models here.
+class CategoriesAdmin(admin.ModelAdmin):
+    list_display = ('icon','name')
+admin.site.register(Categories,CategoriesAdmin)
--- lms/views.py
+++ lms/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import redirect, render
 from django.http import HttpResponse
+from app.models import Categories
 
 def BASE(request):
     return render(request,'base.html')
 
 def HOME(request):
-    return render(request,'Main/home.html')
+
+    category = Categories.objects.all().order_by('id')[0:5]
+    data = {
+        'category':category
+    }
+    return render(request,'Main/home.html',data)
 
 def SINGLE_COURSE(request):
     return render(request,'Main/single_course.html')
